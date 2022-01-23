4d6daf61ab99b42602b75aea0fb079ce6598baf1
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2022-01-22 18:17:03 -0600

    Time: 1 ms (99.90%), Space: 41.8 MB (13.93%) - LeetHub

---
+++ 1528-shuffle-string/1528-shuffle-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        char[] res = new char[indices.length];
+        
+        for(int i = 0; i < s.length(); i++){
+            res[indices[i]] = s.charAt(i);
+        }
+        
+        return String.valueOf(res);
+    }
+}
