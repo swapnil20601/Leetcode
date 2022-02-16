920a64302521a7965555c96c2c2a132ce426cea4
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2022-02-16 16:49:54 -0600

    Time: 1 ms (68.29%), Space: 41.6 MB (27.69%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.java
+++ 709-to-lower-case/709-to-lower-case.java
@@ -1,19 +1,11 @@
 class Solution {
     public String toLowerCase(String s) {
-        String res = "";
+        char[] res = s.toCharArray();
         for(int i = 0; i < s.length(); i++){
-            /*
-            You can use either ternary op or if-else:
-            
-            if(s.charAt(i) >= 65 && s.charAt(i) <= 90){
-                res += (char)(s.charAt(i) + 32);
+            if(res[i] >= 65 && res[i] <= 90){
+                res[i] = (char)(res[i] + 32);
             }
-            else {
-                res += s.charAt(i);
-            }
-            */
-            res += (s.charAt(i) >= 65 && s.charAt(i) <= 90) ? (char)(s.charAt(i) + 32) : s.charAt(i);
         }
-        return res;
+        return new String(res);
     }
 }
