95a20dbd3d6c08dc335ad5e6518b20679b638fc1
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2022-02-16 16:52:07 -0600

    Time: 0 ms (100.00%), Space: 41.5 MB (28.31%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.java
+++ 709-to-lower-case/709-to-lower-case.java
@@ -2,9 +2,8 @@ class Solution {
     public String toLowerCase(String s) {
         char[] res = s.toCharArray();
         for(int i = 0; i < s.length(); i++){
-            if(res[i] >= 65 && res[i] <= 90){
-                res[i] = (char)(res[i] + 32);
-            }
+           
+            res[i] = (res[i] >= 65 && res[i] <= 90) ? (char)(res[i] + 32) : res[i];
         }
         return String.valueOf(res);
     }
