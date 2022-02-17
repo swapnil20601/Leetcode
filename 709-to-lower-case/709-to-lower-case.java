cd2aacf2c8c69cf255c3325f0b4f5922a528bd2c
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2022-02-16 22:09:28 -0600

    Time: 5 ms (14.75%), Space: 42.4 MB (12.32%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.java
+++ 709-to-lower-case/709-to-lower-case.java
@@ -1,5 +1,33 @@
 class Solution {
     public String toLowerCase(String s) {
+        //USING HASHMAP
+        String upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String lower = "abcdefghijklmnopqrstuvwxyz";
+        StringBuilder res = new StringBuilder();
+        
+        Map<Character, Character> map = new HashMap<>();
+        
+        for(int i = 0; i < 26; i++){
+            map.put(upper.charAt(i), lower.charAt(i));
+        }
+        
+        for(char c : s.toCharArray()){
+            if(map.containsKey(c)){
+                res.append(map.get(c));
+            }
+            else{
+                res.append(c);
+            }
+        }
+        
+        return res.toString();
+    }
+}
+
+//try with hashmap
+
+/*  
+public String toLowerCase(String s) {
         char[] res = s.toCharArray();
         for(int i = 0; i < s.length(); i++){
            
@@ -7,6 +35,5 @@ class Solution {
         }
         return new String(res);
     }
-}
 
-//try with hashmap
+*/
