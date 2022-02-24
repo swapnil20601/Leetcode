e5208cbe8fb8eb1b8a94b9be5e019df71a9f7e9b
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2022-02-23 21:01:13 -0600

    Time: 0 ms (100.00%), Space: 40 MB (44.43%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.java
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.java
@@ -1,25 +1,22 @@
 class Solution {
     public String sortSentence(String s) {
-        if(s.length() <= 0) {
-            return s;
+        StringBuilder sb = new StringBuilder();
+        String[] words = s.split(" ");
+        String[] res = new String[words.length];
+        
+        for(String word: words){
+            char ch = word.charAt(word.length()-1);
+            
+            int index = Character.getNumericValue(ch) - 1;;
+            res[index] = word.substring(0,word.length()-1);
         }
         
-        String[] str = s.split(" ");
-        String[] res = new String[str.length];
-        StringBuilder sb = new StringBuilder(); 
-
-        for (String word : str) {
-            char ch = word.charAt(word.length() - 1);
-            int i = Character.getNumericValue(ch) - 1;
-            //int i = ch - '1';
-            res[i] = word.substring(0, word.length() - 1);
-        }
-
-        for (int i = 0; i < res.length-1; i++) {
+        for(int i = 0; i < res.length-1;i++){
             sb.append(res[i]).append(" ");
         }
         
         sb.append(res[res.length-1]);
+        
         return sb.toString();
     }
 }
