673920fbf7150f5404c33cefc6ec6adc8f40cd49
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2022-02-24 19:59:18 -0600

    Time: 1 ms (56.76%), Space: 45.7 MB (20.98%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -1,16 +1,11 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-        int maxVal = 1001;
+        int[] res = new int[2*n];
         
-        for(int i= 0; i < n; i++){
-            nums[i+n] = nums[i] * maxVal + nums[i+n];
+        for(int i = 0 ; i < n; i++){
+            res[2*i] = nums[i];
+            res[(2*i)+1] = nums[i+n];
         }
-        
-        for(int i= 0; i < n; i++){
-            nums[2*i] = nums[i+n] / maxVal;
-            nums[2*i + 1] = nums[i+n] % maxVal;
-        }
-        
-        return nums;
+        return res;
     }
 }
