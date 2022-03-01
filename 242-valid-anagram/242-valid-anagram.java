93dfc05db8f644684966e5bc1837c327e0bcd722
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2022-02-28 20:31:10 -0600

    Time: 12 ms (30.89%), Space: 42.9 MB (53.06%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,14 +1,28 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        char[] s1 = s.toCharArray();
-        char[] t1 = t.toCharArray();
+        if(s == null || t == null || s.length() == 0 || t.length() == 0 || s.length() != t.length()){
+            return false;
+        }
         
-        Arrays.sort(s1);
-        Arrays.sort(t1);
+        Map<Character, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < s.length(); i++){
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0)+1);
+        }
+        
+        for(int i = 0; i < t.length(); i++){
+            char key = t.charAt(i);
+            if(!map.containsKey(key)){
+                return false;
+            }
+            else if(map.get(key) == 1){
+                map.remove(key);
+            }
+            else{
+                map.put(t.charAt(i), map.get(key)-1);
+            }
+        }
 
-        String sortedS1 = String.valueOf(s1);
-        String sortedT1 = new String(t1);
-
-        return sortedS1.equals(sortedT1);
+        return map.size() == 0;
     }
 }
