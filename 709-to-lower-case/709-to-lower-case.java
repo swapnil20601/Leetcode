416ee6323ac329c63bda11951b32a5cf2137b36e
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2022-03-02 14:33:31 -0600

    Time: 5 ms (13.69%), Space: 41.7 MB (42.49%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.java
+++ 709-to-lower-case/709-to-lower-case.java
@@ -1,39 +1,24 @@
 class Solution {
     public String toLowerCase(String s) {
-        //USING HASHMAP
-        String upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String caps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         String lower = "abcdefghijklmnopqrstuvwxyz";
-        StringBuilder res = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
         
         Map<Character, Character> map = new HashMap<>();
-        
-        for(int i = 0; i < 26; i++){
-            map.put(upper.charAt(i), lower.charAt(i));
+        for(int i= 0; i < caps.length();i++){
+            map.put(caps.charAt(i), lower.charAt(i));
         }
         
-        for(char c : s.toCharArray()){
+        for(char c: s.toCharArray()){
             if(map.containsKey(c)){
-                res.append(map.get(c));
+                sb.append(map.get(c));
             }
             else{
-                res.append(c);
+                sb.append(c);
             }
         }
         
-        return res.toString();
+        return new String(sb);
     }
 }
 
-//try with hashmap
-
-/*  
-public String toLowerCase(String s) {
-        char[] res = s.toCharArray();
-        for(int i = 0; i < s.length(); i++){
-           
-            res[i] = (res[i] >= 65 && res[i] <= 90) ? (char)(res[i] + 32) : res[i];
-        }
-        return new String(res);
-    }
-
-*/
