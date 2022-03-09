ef80d2a8f5ffda70e52316a20b72816f7416b6b9
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2022-03-09 09:49:06 -0600

    Time: 2 ms (82.18%), Space: 42.6 MB (58.54%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
@@ -1,16 +1,15 @@
 class Solution {
     public int[] finalPrices(int[] prices) {
         Deque<Integer> stk = new ArrayDeque<>();
-        int[] res = prices.clone();
         
         for(int i = 0; i < prices.length; i++){
             while(!stk.isEmpty() && prices[i] <= prices[stk.peek()]){
-                res[stk.peek()] = prices[stk.peek()] - prices[i];
+                prices[stk.peek()] = prices[stk.peek()] - prices[i];
                 stk.pop();
             }
             stk.push(i);
         }
         
-        return res;
+        return prices;
     }
 }
