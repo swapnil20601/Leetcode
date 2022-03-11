a35077b4bc4ea772ded393801d2562b9d2562003
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2022-03-11 11:17:34 -0600

    Time: 1 ms (16.84%), Space: 46.3 MB (37.50%) - LeetHub

---
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        if(arr.length < 3){
+            return -1;
+        }
+        
+        for(int i = 1 ;i < arr.length - 1; i++){
+            if(arr[i] > arr[i-1] && arr[i] > arr[i+1]){
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+}
