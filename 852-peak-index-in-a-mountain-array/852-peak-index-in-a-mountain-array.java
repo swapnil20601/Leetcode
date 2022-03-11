80139bb57aaf3bea1faa646df3b7aca527d3241c
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2022-03-11 13:35:41 -0600

    Time: 0 ms (100.00%), Space: 46.5 MB (23.32%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
@@ -4,18 +4,46 @@ class Solution {
             return -1;
         }
         
-        if(arr[0] > arr[1]){
-            return 0;
-        }
+        int low = 0;
+        int high = arr.length-1; 
         
-        if(arr[arr.length-1] > arr[arr.length-2]){
-            return arr.length-1;
-        }
         
-        for(int i= 1; i < arr.length-1; i++){
-            if(arr[i] > arr[i-1] && arr[i] > arr[i+1]){
-                return i;
+        while(low <= high){
+            int mid = (high - low)/2 + low;
+            
+            if(mid > 0 && mid < arr.length-1) {
+                 if(arr[mid] > arr[mid-1] && arr[mid] > arr[mid+1]){
+                    return mid;
+                }
+                else if(arr[mid] > arr[mid-1]){
+                    low = mid+1;
+                }
+                else{
+                    high = mid-1;
+                }
             }
+            
+            //here if it was while(low <= high) then we wud have get error bcoz we need atlest 2 2elemnst to compare arr[i] & arr[i+1].
+            //becoz lets say there was only 1 element & both high low are pointing to it, then how will u compare arr[mid] > arr[1] ? 
+            //we will get indexoutof bound exception here
+            else if(mid == 0){
+                if(arr[mid] > arr[1]){
+                    return mid;
+                }
+                else{
+                    return 1;
+                }
+                
+            }
+            else if(mid == arr.length-1){
+                if(arr[mid] > arr[arr.length-2]){
+                   return mid; 
+                }
+                else {
+                    return arr.length-2;
+                }
+            }
+           
         }
         
         return -1;
