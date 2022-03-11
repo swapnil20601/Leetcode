9d46f1f992fd57857afd71946a438b1aa05bbdf8
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2022-03-11 11:00:48 -0600

    Time: 99 ms (21.63%), Space: 61.1 MB (53.80%) - LeetHub

--- 346-moving-average-from-data-stream/346-moving-average-from-data-stream.java
+++ 346-moving-average-from-data-stream/346-moving-average-from-data-stream.java
@@ -11,7 +11,7 @@ class MovingAverage {
     
     public double next(int val) {
         this.sum += val;
-        this.queue.offer(val);
+        this.queue.add(val);
         
         if(this.queue.size() > this.size){
             this.sum -= this.queue.poll();
