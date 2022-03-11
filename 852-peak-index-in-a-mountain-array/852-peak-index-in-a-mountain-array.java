3f99d6f12788f833eee601b20ac6430c8eb10d07
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2022-03-11 12:52:52 -0600

    Time: 1 ms (16.84%), Space: 47 MB (8.69%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
@@ -4,16 +4,20 @@ class Solution {
             return -1;
         }
         
-        int maxSofar = Integer.MIN_VALUE;
-        int maxIndex = -1;
+        if(arr[0] > arr[1]){
+            return 0;
+        }
         
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i] > maxSofar){
-                maxIndex = i;
-                maxSofar = arr[i];
+        if(arr[arr.length-1] > arr[arr.length-2]){
+            return arr.length-1;
+        }
+        
+        for(int i= 1; i < arr.length-1; i++){
+            if(arr[i] > arr[i-1] && arr[i] > arr[i+1]){
+                return i;
             }
         }
         
-        return maxIndex;
+        return -1;
     }
 }
