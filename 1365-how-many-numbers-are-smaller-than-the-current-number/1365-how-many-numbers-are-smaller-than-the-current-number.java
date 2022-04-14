8666e65d4a85eee8a436d1164e5d668ecf120c93
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2022-04-14 08:41:10 -0500

    Time: 18 ms (37.24%), Space: 42.5 MB (87.34%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
@@ -1,27 +1,17 @@
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
-        int[] res = new int[nums.length];
-        int[] freq = new int[101];
+       int[] res = new int[nums.length];
         
-        //stor occurences of each number in nums insode freq array
-        for(int i = 0; i < nums.length; i++){
-            freq[nums[i]]++;
-        }
-        
-        //calculatr prefix sum till end of freq array
-        for(int i = 1; i < 101; i++){
-            freq[i] += freq[i-1];
-        }
-        
-        //update res[] array ith the element just before nums[i]. Beware if nums[i] = 0. Take special care
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] == 0){
-                res[i] = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int count = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (i != j && nums[j] < nums[i]) {
+                    count++;
+                }
             }
-            else {
-                res[i] = freq[nums[i]-1];
-            }
+            res[i] = count;
         }
+
         return res;
     }
 }
