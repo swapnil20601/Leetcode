05a868406039a9e39725404b51a541b9a2da8ac2
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2022-04-14 10:20:15 -0500

    Time: 1 ms (99.54%), Space: 42.4 MB (90.25%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
@@ -1,20 +1,30 @@
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
+        int res[] = new int[nums.length];
         
-        int[] copy = nums.clone();
-        Arrays.sort(copy);
+        int[] freq = new int[101];
         
+        //update frequency of each number in nums[] in freq[]
         for(int i = 0; i < nums.length; i++){
-            if(!map.containsKey(copy[i])){
-                map.put(copy[i], i);
+            freq[nums[i]]++;
+        }
+        
+        //calculate running sum
+        for(int i = 1; i < freq.length; i++){
+            freq[i] = freq[i] + freq[i-1];
+        }
+        
+        //We will 1st look for nums[i] in freq[] & will pick prev index's value for freq[nums[i]] ONLY IF nums[i] != 0.
+        //Otherwise we will simply put 0 if nums[i] = 0 bcoz 0 is the smallest of all as per given constraints. If we 
+        // go for prev index val freq[nums[i]] , then we wiil get -1 & ArrayIndexOutOfBound
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == 0){
+                res[i] = 0;
+            } else {
+                res[i] = freq[nums[i] - 1];
             }
         }
         
-        for(int i = 0; i < nums.length; i++){
-            copy[i] = map.get(nums[i]);
-        }
-        
-        return copy;
+        return res;
     }
 }
