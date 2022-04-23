b746c7087454c22a395a3d7de79ceba8e279d31b
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2022-04-23 17:57:49 -0500

    Time: 1 ms (90.46%), Space: 42.3 MB (41.85%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -1,10 +1,17 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
-        Set<Character> set = new HashSet<>();
-        for(char c: sentence.toCharArray()){
-            set.add(c);
+        boolean[] res = new boolean[26];
+        
+        for(char c : sentence.toCharArray()){
+            res[c-'a'] = true;
         }
         
-        return set.size() == 26;
+        for(boolean flag:res){
+            if(!flag){
+                return false;
+            }
+        }
+        
+        return true;
     }
 }
