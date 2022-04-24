643012f34a32d74e554acd66e17ecf81196fa222
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2022-04-23 19:52:26 -0500

    Time: 0 ms (100.00%), Space: 42.5 MB (80.13%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
@@ -3,13 +3,15 @@ class Solution {
         int sum = 0;
         int n = mat.length;
         
-        for(int i= 0; i<n ;i++){
+        //first find sum of all elements in primary & secondary diagonal
+        for(int i = 0; i < n; i++){
             sum += mat[i][i] + mat[i][n-i-1];
         }
         
-        if(n%2!=0){
-            sum = sum - mat[n/2][n/2];
-            return sum;
+        //if length iof matrix is odd, then remove middle element from total sum
+        //else, if its is even, then return total sum
+        if(n%2 == 1){
+           return sum -= mat[n/2][n/2];
         }
         else {
             return sum;
