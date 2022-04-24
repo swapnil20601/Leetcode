3ed445ac691291fb5fa42ea52a2c348b31bda7a3
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2022-04-24 08:20:45 -0500

    Time: 1 ms (59.85%), Space: 42.3 MB (18.25%) - LeetHub

--- 1180-count-substrings-with-only-one-distinct-letter/1180-count-substrings-with-only-one-distinct-letter.java
+++ 1180-count-substrings-with-only-one-distinct-letter/1180-count-substrings-with-only-one-distinct-letter.java
@@ -1,18 +1,23 @@
 class Solution {
 
     public int countLetters(String s) {
-        int count = 1;
+        if(s.length() == 0 || s == null){
+            return 0;
+        }
+        
         int total = 1;
+        int[] substring_freqy = new int[s.length()];
+        substring_freqy[0] = 1;
         
         for(int i = 1; i < s.length(); i++){
             if(s.charAt(i) == s.charAt(i-1)){
-                count++;
+                substring_freqy[i] = substring_freqy[i-1] + 1;
             }
             else {
-                count = 1;
+                substring_freqy[i] = 1;
             }
             
-            total += count;
+            total += substring_freqy[i];
         }
         
         return total;
