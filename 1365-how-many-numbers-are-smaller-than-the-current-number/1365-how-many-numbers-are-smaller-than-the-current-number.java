cfc914356765268409e841f5de861ddf270798be
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2022-05-02 13:14:56 -0500

    Time: 18 ms (36.12%), Space: 42.6 MB (81.41%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
@@ -1,30 +1,15 @@
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
-        int res[] = new int[nums.length];
+        int n = nums.length, count = 0;
+        int[] res = new int[n];
         
-        int[] freq = new int[101];
-        
-        //update frequency of each number in nums[] in freq[]
-        for(int i = 0; i < nums.length; i++){
-            freq[nums[i]]++;
-        }
-        
-        //calculate running sum
-        for(int i = 1; i < freq.length; i++){
-            freq[i] = freq[i] + freq[i-1];
-        }
-        
-        //We will 1st look for nums[i] in freq[] & will pick prev index's value for freq[nums[i]] ONLY IF nums[i] != 0.
-        //Otherwise we will simply put 0 if nums[i] = 0 bcoz 0 is the smallest of all as per given constraints. If we 
-        // go for prev index val freq[nums[i]] , then we wiil get -1 & ArrayIndexOutOfBound
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] == 0){
-                res[i] = 0;
-            } else {
-                res[i] = freq[nums[i] - 1];
+        for(int i = 0; i < n; i++){
+            count = 0;
+            for(int j = 0;j < n; j++){
+                if(j != i && nums[j] < nums[i]) count++;
             }
+            res[i] = count;
         }
-        
         return res;
     }
 }
