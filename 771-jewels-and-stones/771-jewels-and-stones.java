689cacc3be4053f0c188136ef0a5429b80582481
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2022-05-02 12:23:29 -0500

    Time: 3 ms (27.83%), Space: 43.1 MB (6.27%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.java
+++ 771-jewels-and-stones/771-jewels-and-stones.java
@@ -1,20 +1,14 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
         int count = 0;
-        /*
-        we create array of 128 size bcoz A-z & a-z characters lies bewteen 65- 122 range. 
-        so we need to use array with size at least = 123
-        */
-        int[] frequency = new int[128]; 
-        
-        for(char s : stones.toCharArray()){
-            frequency[s]++;
+        Map<Character,Integer> map = new HashMap<>();
+        for(char s:stones.toCharArray()){
+            map.put(s,map.getOrDefault(s,0)+1);
         }
         
-        for(char j :jewels.toCharArray()){
-            count = count + frequency[j];
+        for(char j:jewels.toCharArray()){
+            count += map.getOrDefault(j,0);
         }
-        
         return count;
     }
 }
