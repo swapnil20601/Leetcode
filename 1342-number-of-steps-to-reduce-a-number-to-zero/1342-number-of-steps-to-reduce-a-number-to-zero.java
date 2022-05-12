164c536f8c3e8f3ab5b6a19d2371fd865174a08f
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2022-05-12 11:19:22 -0500

    Time: 0 ms (100.00%), Space: 39.2 MB (86.86%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
@@ -3,15 +3,15 @@ class Solution {
         int steps = 0;
         
         while(num > 0){
-            if((num & 1) == 1){
-                num -= 1;
+            if(num % 2 == 0){
+                num = num/2;
             }
-            else {
-                num >>= 1;
+            else{
+                num -= 1;
             }
             steps++;
         }
         
         return steps;
-        }
+    }
 }
