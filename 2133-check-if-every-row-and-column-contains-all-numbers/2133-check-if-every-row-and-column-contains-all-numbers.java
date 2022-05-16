576338f9a2069337d6a045a9a028502c2d29ac09
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2022-05-16 10:16:28 -0500

    Time: 8 ms (79.64%), Space: 71.8 MB (58.73%) - LeetHub

---
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean checkValid(int[][] matrix) {
+        int n = matrix.length;
+        
+        for(int r = 0; r < n; r++){
+            boolean[] rowVisited = new boolean[n+1];
+            boolean[] colVisited = new boolean[n+1];
+            
+            for(int c = 0; c < n; c++){
+                int rowVal = matrix[r][c];
+                int colVal = matrix[c][r];
+                
+                if(rowVisited[rowVal] == true || colVisited[colVal] == true){
+                    return false;
+                }
+                
+                rowVisited[rowVal] = true;
+                colVisited[colVal] = true;
+            }
+        }
+        
+        return true;
+    }
+}
