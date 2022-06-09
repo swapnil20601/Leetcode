540feac0931c40c429bcdc7784c3c98632c6e260
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2022-06-09 12:32:14 -0500

    Time: 4 ms (51.42%), Space: 42.3 MB (44.54%) - LeetHub

--- 682-baseball-game/682-baseball-game.java
+++ 682-baseball-game/682-baseball-game.java
@@ -1,34 +1,33 @@
 class Solution {
     public int calPoints(String[] ops) {
-         int sum = 0;
-        Stack<Integer> stk = new Stack<>();
+        int sum = 0;
+        LinkedList<Integer> list = new LinkedList<>();
 
         for (String obj : ops) {
             if(obj.equals("C")){
-                sum -= stk.pop();
+                sum -= list.removeLast();
             }
             else if(obj.equals("D")){
-                int lastEle = stk.peek();
+                int lastEle = list.getLast();
                 int doubleVal = lastEle + lastEle;// we dont do 2 * lastEle becoz consider TC where lastEle = -2 & -2 * -2 = 4. But we want -4 after doubling -2
-                stk.push(doubleVal);
+                list.addLast(doubleVal);;
                 sum += doubleVal;
             }
             else if(obj.equals("+")){
-                int first = stk.pop();
-                int second = stk.pop();
+                int first = list.removeLast();
+                int second = list.removeLast();
                 int res = first+second;
-                stk.push(second);
-                stk.push(first);
-                stk.push(res);
+                list.addLast(second);
+                list.addLast(first);
+                list.addLast(res);
                 sum += res;
             }
             else {
                 int ele = Integer.valueOf(obj);
-                stk.push(ele);
+                list.addLast(ele);
                 sum += ele;
             }
         }
-        
         return sum;
     }
 }
