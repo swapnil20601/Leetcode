aeb3df684ad3e6a5e3f73ab9ec450ee5ad0d5122
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2022-06-15 17:08:48 -0500

    Time: 3 ms (52.55%), Space: 42.6 MB (29.93%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.java
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.java
@@ -6,10 +6,8 @@ class Solution {
         
         Arrays.sort(arr);
         
-        int diff = Math.abs(arr[0] - arr[1]);
-        
         for(int i = 1; i < arr.length-1; i++){
-            if(Math.abs(arr[i] - arr[i+1]) != diff){
+            if(arr[i] - arr[i-1] != arr[i+1] - arr[i]){
                 return false;
             }
         }
