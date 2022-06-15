a64f4f0c4bf355ebb90d84aa45be67c05356bda3
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2022-06-15 18:51:13 -0500

    Time: 7 ms (37.12%), Space: 44.8 MB (21.50%) - LeetHub

--- 414-third-maximum-number/414-third-maximum-number.java
+++ 414-third-maximum-number/414-third-maximum-number.java
@@ -1,45 +1,28 @@
 class Solution {
     public int thirdMax(int[] nums) {
-        long firstMax = Long.MIN_VALUE;
-        long secondMax = Long.MIN_VALUE;
-        long thirdMax = Long.MIN_VALUE;
         
-        if(nums.length == 1){
-            return nums[0];
-        }
+        Set<Integer> set = new HashSet<>();
         
-        if(nums.length == 2){
-            return Math.max(nums[0], nums[1]);
+        for(int val : nums){
+            set.add(val);
         }
         
-        for(int i: nums){
-            //we dont want duplicate nums in 1st/2nd/3rdmax. So we check if i == to either of them, then we ignore i & moove forward
-            if(i==firstMax || i==secondMax || i==thirdMax){
-                continue;
-            }
-            
-            if(i > firstMax){
-                thirdMax = secondMax;
-                secondMax = firstMax;
-                firstMax = i;
-            }
-            else if(i > secondMax){
-                thirdMax = secondMax;
-                secondMax = i;
-            }
-            else if(i > thirdMax){
-                thirdMax = i;
-            }
+        int firstMax = Collections.max(set);
+        
+        //If 3rd max does not exists, then just return maximum as per question
+        if(set.size() < 3){
+            return firstMax;
         }
         
-        //cosniuder nums =[1,2,2,2,2] 0r [2,1,2,1]. Here we dont get 3rdMax. 3rdMax = -inf. 
-        //So in that we have to return largest of all as per question(see 2nd example in Question). So we take care of that here
-        if(thirdMax == Long.MIN_VALUE) {
-            return (int)firstMax;
-        }
-        else {
-            return (int)thirdMax;
-        }
+        //Otherwise continue finding 2nd & 3rd max
+        set.remove(firstMax);
+        
+        //find 2nd max and delete it
+        int secondMax = Collections.max(set);
+        set.remove(secondMax);
+        
+        //find 3rd max & now return it
+        return Collections.max(set);
     }
 }
 
