59c013da24e1d7dc5b00046309c13421e7ba6019
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2022-06-20 15:25:33 -0500

    Time: 3 ms (81.47%), Space: 44.3 MB (22.36%) - LeetHub

--- 1436-destination-city/1436-destination-city.java
+++ 1436-destination-city/1436-destination-city.java
@@ -1,17 +1,18 @@
 class Solution {
     public String destCity(List<List<String>> paths) {
-        Set<String> city = new HashSet<>();
+        Map<String, String> city = new HashMap<>();
         
         for(List<String> path: paths){
-            city.add(path.get(0));
+            city.put(path.get(0), path.get(1));
         }
         
-        for(List<String> path: paths){
-            if(!city.contains(path.get(1))){
-                return path.get(1);
+        for(String destination: city.values()){
+            if(!city.containsKey(destination)){
+                return destination;
             }
         }
-        
         return "";
     }
 }
+
+
