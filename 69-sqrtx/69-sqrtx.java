590c1760ae5103f0a8d71ee1597b8333335b1d2e
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2022-06-21 14:55:21 -0500

    Time: 1 ms (99.99%), Space: 39.8 MB (84.23%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -1,10 +1,25 @@
 class Solution {
     public int mySqrt(int x) {
-        long i = 1;
-        while(i*i <= x){
-            i++;
+        int ans = 0;
+        int low = 1;
+        int high = x;
+        
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            int half = x/mid;
+            
+            if(mid == half){
+                return mid;
+            }
+            else if(mid < half){
+                ans = mid;
+                low = mid+1;
+            }
+            else {
+                high = mid-1;
+            }
         }
         
-        return (int)i-1;
+        return ans;
     }
 }
