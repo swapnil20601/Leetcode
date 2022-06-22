09d208c06af10738f7aeb36d84bf1f7791bd690b
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2022-06-21 20:24:33 -0500

    Time: 1 ms (99.83%), Space: 42.5 MB (83.74%) - LeetHub

--- 541-reverse-string-ii/541-reverse-string-ii.java
+++ 541-reverse-string-ii/541-reverse-string-ii.java
@@ -1,20 +1,17 @@
 class Solution {
     public String reverseStr(String s, int k) {
-        char[] arr = s.toCharArray();
-        int j = 0;
+        char[] res = s.toCharArray();
         
-        for(int i = 0; i < arr.length; i = i+2*k){
-            j = Math.min(arr.length-1, i+k-1);
-            
-            reverseChars(arr, i, j);
-            
+        for(int i = 0; i < res.length; i = 2*k+i){
+            int j = Math.min(i+k-1, s.length()-1);
+            reverseSubString(res,i,j);
         }
         
-        return new String(arr);
+        return new String(res);
     }
     
-    public void reverseChars(char[] arr, int i, int j){
-        while(i < j){
+    private void reverseSubString(char[] arr, int i, int j){
+        while(i<j){
             char temp = arr[i];
             arr[i++] = arr[j];
             arr[j--] = temp;
