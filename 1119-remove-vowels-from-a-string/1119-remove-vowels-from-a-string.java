8d0c36b35e37c03ac7bd51620e847e654e74d0a3
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2022-06-24 08:17:33 -0500

    Time: 0 ms (100.00%), Space: 42.2 MB (35.68%) - LeetHub

--- 1119-remove-vowels-from-a-string/1119-remove-vowels-from-a-string.java
+++ 1119-remove-vowels-from-a-string/1119-remove-vowels-from-a-string.java
@@ -1,19 +1,16 @@
 class Solution {
     public String removeVowels(String s) {
         StringBuilder sb = new StringBuilder();
-        Set<Character> set = new HashSet<>();
-        set.add('a');
-        set.add('e');
-        set.add('i');
-        set.add('o');
-        set.add('u');
         
-        for(int i = 0 ; i < s.length(); i++){
-            if(!set.contains(s.charAt(i))){
-                sb.append(s.charAt(i));
+        for(char c : s.toCharArray()){
+            if(c != 'a' && c != 'e' && c!= 'i' && c != 'o' && c != 'u'){
+                sb.append(c);
             }
         }
-        
         return sb.toString();
     }
 }
+
+// Checkout this solution where you do not use extra space: https://leetcode.com/problems/remove-vowels-from-a-string/discuss/452476/Java-StringBuffer-0ms-with-written-approach-to-answering-in-interview. 
+//Also check the solution how its implemneted in 1st comment of this soln
+// Basically whenver possible use StringBuffer
