114347e1300c3eb82bf59fb3ef4569afeceb2676
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2024-04-04 14:29:08 -0500

    Time: 88 ms (22.69%), Space: 27.1 MB (74.35%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -5,38 +5,35 @@
 #         self.next = None
 
 class Solution:
-    def getLen(self,head):
-        size = 0
-        curr = head
-        while curr:
-            size += 1
-            curr = curr.next
-        return size
-        
-
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        len1 = self.getLen(headA)
-        len2 = self.getLen(headB)
         t1 = headA
         t2 = headB
-        
-        if len2 > len1:
 
-            d = len2-len1
-            for _ in range(d):
-                t2 = t2.next
-        else:
-            d = len1-len2
-            for _ in range(d):
-                t1 = t1.next
-        
-        while t1 and t2:
-            if t1 is t2:
-                return t1
+        #Edge cases: if eother of the 2 LLs is empty
+        if not t1 or not t2:
+            return None 
+
+        #Run the loop until both ptrs are pointing on different nodes & not on same npode
+        while t1 is not t2:
             t1 = t1.next
             t2 = t2.next
+
+            #If t1 & t2 meet, then it is the intersection point
+            if t1 == t2:
+                return t1
+
+            #If t1 reached last node, then move it to head2
+            if t1 is None:
+                t1 = headB
+            
+            #If t2 reached last node, then move it to head1
+            if t2 is None:
+                t2 = headA
         
         return t1
+
+
+        
         
 
         
