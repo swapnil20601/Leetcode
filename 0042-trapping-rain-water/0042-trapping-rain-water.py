1e95313fc4f766662e61b69842328bac7889ad96
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2024-04-25 20:59:35 -0500

    Time: 92 ms (89.76%), Space: 18.5 MB (39.51%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -2,27 +2,25 @@ class Solution:
     def trap(self, height: List[int]) -> int:
         n = len(height)
         ans = 0
-        left_max = [0] * n
-        right_max = [0] * n
+        l = 0
+        r = n-1
+        left_max = -1
+        right_max = -1
 
-        #Set 1st ele of left_max[] to 1st ele of arr[] 
-        left_max[0] = height[0]
-
-        #Fill in the left_max[] starting from 1st index
         for i in range(1,n):
-            left_max[i] = max(height[i],left_max[i-1])
-        
-        #Set last ele of right_max[] to last ele of arr[] 
-        right_max[n-1] = height[n-1]
-
-        #Fill in the right_max[] starting from n-1 to 0th index
-        for i in range(n-2,-1,-1):
-            right_max[i] = max(height[i],right_max[i+1])
-
-        #Calculate total water trapped 
-        for i in range(n):
-            ans += min(left_max[i],right_max[i]) - height[i]            
-        
+            if height[l] <= height[r]:
+                if height[l] > left_max:
+                    left_max = height[l]
+                else:
+                    ans += left_max-height[l]
+                l += 1
+            else:
+                if height[r] > right_max:
+                    right_max = height[r]
+                else:
+                    ans += right_max-height[r]
+                r -= 1
+                
         return ans
                 
 
