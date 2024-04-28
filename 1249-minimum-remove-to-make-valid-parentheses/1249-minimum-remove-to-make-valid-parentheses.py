d8dbae3fb09c9fe6bd22b463811b372da3d85b84
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2024-04-28 12:24:13 -0500

    Time: 88 ms (36.52%), Space: 19.1 MB (16.03%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -1,26 +1,41 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        hash = set()
-        stack = []
-        out =''
+        output = 0
+        ans = []
+        
         for i in range(len(s)):
-            if s[i] == '(':
-                stack.append(i)
-            elif s[i] == ')':
-                if stack:
-                    stack.pop()
-                else:
-                    hash.add(i)
+            if s[i] >= 'a' and s[i] <= 'z':
+                ans.append(s[i])
+            elif s[i] == '(':
+                output += 1
+                ans.append(s[i])
+            else:
+                if output > 0:
+                    output -= 1
+                    ans.append(s[i])
+    
+        
+        #Now iterate from right--> left to handle case 3 to remove extra open bracket
 
-        #Dry run over ex 1 to understand why we do this
-        if stack:
-            while stack:
-                hash.add(stack.pop())
-        
-        for i in range(len(s)):
-            if i not in hash:
-                out += s[i]
-        
-        return out
+        #reset output
+        output = 0
+        res = [''] * len(ans)
+
+        #yaha galti ki thi
+        for i in range(len(ans)-1,-1,-1):
+            if ans[i] >= 'a' and ans[i] <= 'z':
+                res[i] = ans[i]
+            elif ans[i] == ')':
+                output += 1
+                res[i] = ans[i]
+            else:
+                if output > 0:
+                    output -= 1
+                    res[i] = ans[i]
+
+        return ''.join(res)            
+
+                
+#First iterate from left--> right to handle case 1 & 2 to remove extra closed bracket
 
         
