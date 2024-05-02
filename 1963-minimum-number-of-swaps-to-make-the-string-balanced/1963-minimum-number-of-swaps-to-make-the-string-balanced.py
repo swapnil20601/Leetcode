ab12e170d8a4e0ac3a55f777c33d3824bf7a2a85
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2024-05-02 12:34:56 -0500

    Time: 212 ms (58.01%), Space: 34.1 MB (5.74%) - LeetHub

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.py
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.py
@@ -4,35 +4,23 @@ class Solution:
         if len(s) & 1:
             return -1
         
-        count = 0
         stack = []
         for ch in s:
             if ch == '[':
                 stack.append(ch)
-#If ch is closing bracket, then check if stack is not emoty? 
-#If it is emoty, then push ]. 
-#If it is not emoty, then check if stack's top = opening bracket? If yes, then only pop. This
-#is bcoz it is possible that stack's top = closing bracket. Why? -> you must have pushed
-#closing bracket when stack was empty
             else:
                 if stack and stack[-1] == '[':
                     stack.pop()
                 else:
                     stack.append(ch)
 
-        #if stack is not empty, it means there are unbalanced brackets. So count reversals
-        # while stack:
-        #     s1 = stack.pop()
-        #     s2 = stack.pop()
-        #     if s1 == s2:
-        #         count += 1
-        #     else:
-        #         count += 2
+        open = len(stack)//2
 
-        totalbrackets = len(stack)
-        closedbrackets = totalbrackets//2
+        if open & 1:
+            return (open//2) + 1
+        else:
+            return open//2
+
         
-        return (closedbrackets+1)//2
-            
 
         
