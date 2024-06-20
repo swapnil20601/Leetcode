359744a37f4dfbce50e050de88435f692d4fe5a1
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2024-06-20 16:14:42 -0500

    Time: 54 ms (83.12%), Space: 18.4 MB (75.15%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -5,33 +5,41 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def solve(self, preorder, preStart, preEnd, inorder, inStart, inEnd, inMap):
+    def searchInorder(self,inorder,element):
+        for i in range(len(inorder)):
+            if inorder[i] == element:
+                return i
+        return -1
+
+    def solve(self, preorder,inorder,preStart,preEnd,inStart,inEnd,map):
+        #Base Case
         if preStart > preEnd or inStart > inEnd:
             return None
 
-        # Create a new TreeNode with value at the current preorder index
+        #Solve 1 case
+        #Create root node from preorder[] with value at wherever you are currently standing
         root = TreeNode(preorder[preStart])
 
-        # Find the index of the current root value in the inorder traversal
-        inRoot = inMap[root.val]
+        #Search for the root node in inorder[] to build its left & right subtree & return its index
+        inRoot = map[root.val]
 
-        # Calculate the number of elements in the left subtree
         numsLeft = inRoot - inStart
 
+        #Constructing left & right subtree will be done by Recursion
         # Recursively build the left subtree
-        root.left = self.solve(preorder, preStart + 1, preStart + numsLeft,
-                                    inorder, inStart, inRoot - 1, inMap)
-
+        root.left = self.solve(preorder,inorder,preStart+1,preStart+numsLeft,inStart,inRoot-1,map)
+ 
         # Recursively build the right subtree
-        root.right = self.solve(preorder, preStart + numsLeft + 1, preEnd,
-                                     inorder, inRoot + 1, inEnd, inMap)
+        root.right = self.solve(preorder,inorder,preStart+numsLeft+1,preEnd,inRoot+1,inEnd,map)
 
         # Return the current root node
         return root
     
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        inMap = {val: idx for idx, val in enumerate(inorder)}
 
-        return self.solve(preorder, 0, len(preorder)-1, inorder, 0, len(inorder)-1, inMap)
+        # Create a map to store indices of elements in the inorder traversal
+        map = {val: idx for idx, val in enumerate(inorder)}
+
+        return self.solve(preorder, inorder, 0,len(inorder)-1, 0, len(inorder)-1,map)
 
         
