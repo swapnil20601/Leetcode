0e7c6329ab3e24e24ae6baab8a1be7417b50f527
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2024-07-05 13:57:56 -0500

    Time: 49 ms (36.97%), Space: 17.9 MB (9.77%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -5,27 +5,21 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def height(self,root):
+    def solve(self,root,res):
         if not root:
-            return 0
-        left_ht = self.height(root.left)
-        right_ht = self.height(root.right)
-        return 1 + max(left_ht,right_ht)
+            return (0,True)
+        
+        left_ht = self.solve(root.left,res)
+        right_ht = self.solve(root.right,res)
+        diff = abs(left_ht[0] - right_ht[0])
+        ht = 1 + max(left_ht[0], right_ht[0])
+        return (ht,left_ht[1] and right_ht[1] and diff <= 1)
+
+        # left_ht,l_bal = self.solve(root.left,res)
+        # right_ht,r_bal = self.solve(root.right,res)
+        # return (1+max(left_ht,right_ht), l_bal and r_bal and abs(left_ht - right_ht) <= 1)
+        
 
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        #Base Case
-        if not root:
-            return True
-
-        #I will solve case for current node
-        left_ht = self.height(root.left)
-        right_ht = self.height(root.right)
-        diff = abs(left_ht - right_ht) <= 1
-
-        #Find leftKaAns & rightKaAns using Recursion
-        leftKaAns = self.isBalanced(root.left)
-        rightKaAns = self.isBalanced(root.right)
-
-
-        return leftKaAns and rightKaAns and diff
-        
+        ans = self.solve(root,(0,True))
+        return ans[1]
