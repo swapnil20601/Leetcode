e1e8e853229441b5c8f186057833747916cedc18
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2024-07-08 09:52:34 -0500

    Time: 54 ms (48.87%), Space: 16.8 MB (77.15%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.py
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -7,26 +7,26 @@
 class Solution:
     def __init__(self):
         self.count = 0
-        self.k = 0
-        self.h = defaultdict(int)
 
-    def dfs(self, node, currSum):
+    def dfs(self, node, map, target, currSum):
         if not node:
             return
         
         currSum += node.val
-        if currSum == self.k:
+        if currSum == target:
             self.count += 1
-        self.count += self.h[currSum - self.k]
-        self.h[currSum] += 1
-        self.dfs(node.left, currSum)
-        self.dfs(node.right, currSum)
-        self.h[currSum] -= 1
+        
+        if currSum - target in map:
+            self.count +=  map[currSum - target]
+        map[currSum] += 1
+        self.dfs(node.left, map, target, currSum)
+        self.dfs(node.right, map, target, currSum)
+        map[currSum] -= 1
 
 
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
-        self.k = targetSum
-        self.dfs(root, 0)
+        map = defaultdict(int)
+        self.dfs(root,map,targetSum, 0)
         return self.count
         
         
