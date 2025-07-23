c7a519e375b32c7b5a2b7c5de336da731b861792
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2025-07-22 19:37:24 -0500

    Update 0001-two-sum.java

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,5 +1,14 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+
+
+        //Better: Sorting & 2-ptr => TC: O(n log n) & SC = O(1)
+        //But this will give incorrect ouput because after sorting array, the indices will change
+
+
+
+
+        //Optimal: HashMap  => TC: O(N) & SC = O(N)
         HashMap<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i < nums.length; i++){
             int num = target - nums[i];
