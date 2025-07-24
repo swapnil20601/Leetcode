441a4ebba172d55eedeb37accae7e5182ddbc1be
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2025-07-23 20:08:24 -0500

    Time: 1 ms (99.87%), Space: 46.4 MB (25.76%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,6 +1,11 @@
 class Solution {
     public int singleNumber(int[] nums) {
+
+        /* 
+        Better Soln: Using HashMAp => TC: O(N*logM) + O(M) & SC: O(M) where M is size of hashmap
+        
         HashMap<Integer,Integer> map = new HashMap<>();
+
         for(int x:nums){
             int val = map.getOrDefault(x,0) + 1;
             map.put(x,val);
@@ -14,5 +19,16 @@ class Solution {
             }
         }
         return -1;
+        
+        
+        
+        */
+
+        //Optimal: Using XOR: TC = O(N)
+        int xor = 0;
+        for(int ele : nums){
+            xor ^= ele;
+        }
+        return xor;
     }
 }
