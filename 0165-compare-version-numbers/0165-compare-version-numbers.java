bb6027f6430f6931c96c50d11463c8cfea8cad38
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2026-03-23 17:08:56 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.java
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -1,16 +1,16 @@
 class Solution {
     public int compareVersion(String version1, String version2) {
-        String[] v1 = version1.split("\\.");
-        String[] v2 = version2.split("\\.");
-        int i = 0, j = 0, n1 = v1.length, n2 = v2.length;
+        int i = 0, j = 0, n1 = version1.length(), n2 = version2.length();
 
         while(i < n1 || j < n2){
             int a = 0, b = 0;
-            if(i < n1){
-                a = Integer.parseInt(v1[i]);
+            while(i < n1 && version1.charAt(i) != '.'){
+                a = a * 10 + (version1.charAt(i) - '0');
+                i++;
             }
-            if(j < n2){
-                b = Integer.parseInt(v2[j]);
+            while(j < n2 && version2.charAt(j) != '.'){
+                b = b * 10 + (version2.charAt(j) - '0');
+                j++;
             }
 
             if(a < b){
