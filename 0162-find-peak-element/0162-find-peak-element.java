534a1cce579c96e09305a676d602804ce4feaf7e
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2026-03-27 10:40:09 -0500

    Time: 0 ms (100%), Space: 44 MB (92%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int n = nums.length;
+        if((n == 1) || (nums[0] > nums[1])){
+            return 0;
+        }
+
+        for(int i = 1; i < n-1; i++){
+            if(nums[i+1] < nums[i]){
+                return i;
+            }
+        }
+        return n-1;
+    }
+}
