cf6a7a100ccaa44083da197f52cc0dc2b61b94e7
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2026-03-27 11:03:06 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -5,11 +5,27 @@ class Solution {
             return 0;
         }
 
-        for(int i = 1; i < n-1; i++){
-            if(nums[i+1] < nums[i]){
-                return i;
+        if(nums[n-1] > nums[n-2]) return n-1;
+
+        int low = 1, high = n-2;
+        while(low <= high){
+            int mid = low +(high-low)/2;
+            if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]) {
+                return mid;
+            }
+            else if(nums[mid] > nums[mid-1] && nums[mid] < nums[mid+1]) {
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
             }
         }
-        return n-1;
+        return -1;
     }
 }
+
+
+
+
+
+
