ba04bdcfeb076b2b60f00c916adaed4c50fb37ce
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2026-03-28 11:07:44 -0500

    Time: 1604 ms (5.01%), Space: 56.2 MB (96.31%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,26 +1,23 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         int n = nums.length;
-        Set<List<Integer>> tripletSet = new HashSet<>();
-        Set<Integer> set = new HashSet<>();
-
-        for(int i = 0;i < n; i++){
-            for(int j = i+1; j < n;j++){
-                int target = -1*(nums[i] + nums[j]);
+        Set<List<Integer>> hashSet = new HashSet<>();
+        for(int i = 0; i < n-1; i++){
+            Set<Integer> set = new HashSet<>();
+            for(int j = i+1; j < n; j++){
+                int target = -(nums[i] + nums[j]);
                 if(set.contains(target)){
                     List<Integer> temp = new ArrayList<>();
                     temp.add(nums[i]);
                     temp.add(nums[j]);
                     temp.add(target);
                     Collections.sort(temp);
-                    tripletSet.add(temp);
+                    hashSet.add(temp);
                 }
                 set.add(nums[j]);
             }
-            set.clear();
         }
-
-        List<List<Integer>> res = new ArrayList<>(tripletSet);
-        return res;
+        List<List<Integer>> res = new ArrayList<>(hashSet);
+        return res; 
     }
 }
