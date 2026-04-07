69a7991269655eca6d68a5941a52027cdad34146
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2026-04-07 12:24:45 -0500

    Time: 1 ms (94.39%), Space: 43.8 MB (33.08%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        if(!(str1+str2).equals(str2+str1)){
+            return "";
+        }
+
+        int size = gcd(str1.length(), str2.length());
+        return str1.substring(0,size); // OR you can also return str2.substring(0,size), it does not matter since both strings will have same substring "x"
+    }
+
+    private int gcd(int a, int b){
+        while(b != 0){
+            int temp = a;
+            a = b;
+            b = temp % b;
+        }
+        return a;
+    }
+}
