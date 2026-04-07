b42dc66fd06ff894172baf582a293e66efc161e3
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2026-04-07 16:54:46 -0500

    Time: 99 ms (67.35%), Space: 51.2 MB (44.02%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int countPrimes(int n) {
+        int count = 0;
+        boolean[] arr = new boolean[n];
+        Arrays.fill(arr,true);
+        
+        for(int i = 2; i*i < n; i++){
+            if(arr[i]){
+                for(int j = i*i; j < n; j+= i){
+                    arr[j] = false;
+                }
+            }
+        }
+
+
+        for(int i = 2; i < n; i++){
+            if(arr[i]){
+                count++;
+            }
+        }
+        return count;
+    }
+}
