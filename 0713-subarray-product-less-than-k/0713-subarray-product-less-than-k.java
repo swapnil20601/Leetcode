8e0db55e3e2b5ffe88d4f120bcf665e994b0def3
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2026-04-09 18:13:40 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
@@ -1,17 +1,27 @@
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
+
+        /* This check is important for edge case.
+        Try the following code without this check on arr = [1,2,3], k = 0 or k = 1.
+        You will see that only "i" keeps moving forward & gives IndexOutOfBound error
+         */
+        if(k <= 1) return 0;
+
         int ans = 0;
         int n = nums.length;
-        for(int i = 0; i < n; i++){
-            int prod = 1;
-            for(int j = i; j < n; j++){
-                prod *= nums[j];
-                if(prod >= k){
-                    break;
-                }
-                ans++;
+        int prod = 1;
+        int i = 0, j = 0;
+
+        while(j < n){
+            prod *= nums[j];
+            while(prod >= k){
+                prod /= nums[i];
+                i++;
             }
+            ans = ans + (j-i+1);
+            j++;
         }
+        
         return ans;
     }
 }
