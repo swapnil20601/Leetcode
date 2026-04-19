c07dab2713aa2ab5725615204dda948ab8c20e46
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2026-04-19 11:47:56 -0500

    Time: 4 ms (74.84%), Space: 45.3 MB (38.13%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,20 +1,45 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
         int n1 = nums1.length, n2 = nums2.length;
+
         int[] ans = new int[n1];
-        Arrays.fill(ans,-1);
-        for(int i = 0; i < n1; i++){
-            for(int j = 0; j < n2; j++){
-                if(nums1[i] == nums2[j]){
-                    for(int k = j+1; k < n2; k++){
-                        if(nums2[k] > nums1[i]){
-                            ans[i] = nums2[k];
-                            break;
-                        }
-                    }
-                }
-            }
+
+        //Returns array of next greater element to right of every element in nums2
+        int[] nger = nextGreaterElementRight(nums2);
+
+        //Fill map with key = nums2 element & val = its next greater element to right
+        for(int i = 0; i < n2; i++){
+            map.put(nums2[i], nger[i]);
         }
+
+        //Fill in ans[] with the appropriate values from hashmap
+        for(int i = 0; i < n1; i++){
+            ans[i] = map.get(nums1[i]);
+        }
+
         return ans;
+    }
+
+    private int[] nextGreaterElementRight(int[] arr){
+        int n = arr.length;
+        int[] res = new int[n];
+
+        //Stack stores indexs and not elements itself. Make it a practice
+        Stack<Integer> st = new Stack<>();
+
+        for(int i = n-1; i >= 0; i--){
+            while(!st.isEmpty() && arr[i] > arr[st.peek()]){
+                st.pop();
+            }
+
+            if(st.isEmpty())
+                res[i] = -1;
+            else
+                res[i] = arr[st.peek()];
+            
+            st.push(i);
+        }
+        return res;
     }
 }
