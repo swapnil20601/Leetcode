a7b7cf5f2a07e42e26674237ffc945883c896068
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2026-05-05 08:31:51 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,13 +1,12 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int n = nums.length;
-        HashMap<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i < n; i++){
-            int val = target - nums[i];
-            if(map.containsKey(val)){
-                return new int[]{i,map.get(val)};
+            for(int j = i+1; j < n; j++){
+                if(nums[i] + nums[j] == target){
+                    return new int[]{i,j};
+                }
             }
-            map.put(nums[i], i);
         }
         return new int[]{-1,-1};
     }
