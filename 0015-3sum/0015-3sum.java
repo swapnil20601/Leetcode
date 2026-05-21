009b5181aec24bd2d3bbc2e7d6deed1a3144379f
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2026-05-21 13:51:59 -0500

    Time: 27 ms (98.87%), Space: 60.5 MB (7.4%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,23 +1,43 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
         int n = nums.length;
-        Set<List<Integer>> hashSet = new HashSet<>();
-        for(int i = 0; i < n-1; i++){
-            Set<Integer> set = new HashSet<>();
-            for(int j = i+1; j < n; j++){
-                int target = -(nums[i] + nums[j]);
-                if(set.contains(target)){
+
+        Arrays.sort(nums);
+        
+        for(int i = 0; i < n; i++){
+            if(i !=0 && nums[i-1] == nums[i]){
+                continue;
+            }
+
+            int j = i+1, k = n-1;
+
+            while(j < k){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum < 0){
+                    j++;
+                }
+                else if(sum > 0){
+                    k--;
+                }
+                else{
                     List<Integer> temp = new ArrayList<>();
                     temp.add(nums[i]);
                     temp.add(nums[j]);
-                    temp.add(target);
-                    Collections.sort(temp);
-                    hashSet.add(temp);
+                    temp.add(nums[k]);
+                    result.add(temp);
+                    j++;
+                    k--;
+                    while(j < k && nums[j] == nums[j-1]){
+                        j++;
+                    }
+                    while(j < k && nums[k] == nums[k+1]){
+                        k--;
+                    }
                 }
-                set.add(nums[j]);
             }
         }
-        List<List<Integer>> res = new ArrayList<>(hashSet);
-        return res; 
+
+        return result;
     }
 }
