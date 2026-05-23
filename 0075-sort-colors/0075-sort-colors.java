874c358cdc817c3219e37aa35857a2f3f3fa4637
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2026-05-23 11:15:46 -0500

    Time: 0 ms (100%), Space: 43.8 MB (9.78%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,32 +1,27 @@
 class Solution {
     public void sortColors(int[] nums) {
         int n = nums.length;
-        int zeros = 0, ones = 0, twos = 0;
+        int start = 0, curr = 0, end = n-1;
 
-        for(int i = 0; i < n; i++){
-            if(nums[i] == 0){
-                zeros++;
+        while(curr <= end){
+            if(nums[curr] == 2){
+                swap(nums,curr,end);
+                end--;
             }
-            else if(nums[i] == 1){
-                ones++;
+            else if(nums[curr] == 0){
+                swap(nums,curr,start);
+                start++;
+                curr++;
             }
             else{
-                twos++;
+                curr++;
             }
         }
+    }
 
-        //copy back count of 0s, 1s & 2s to original nums[]
-        int x = 0;
-        for(int j = 0; j < zeros; j++){
-            nums[x++] = 0;
-        }
-
-        for(int j = 0; j < ones; j++){
-            nums[x++] = 1;
-        }
-
-        for(int j = 0; j < twos; j++){
-            nums[x++] = 2;
-        }
+    private void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
