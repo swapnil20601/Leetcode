bec72c2eb09b96f22cb9991a8e31d122463d9570
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2026-05-23 10:16:44 -0500

    Time: 0 ms (100%), Space: 43.3 MB (87.78%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,26 +1,32 @@
 class Solution {
     public void sortColors(int[] nums) {
         int n = nums.length;
-        int[] temp = new int[n];
-        
-        //Initialize temp[] with all 1's
-        Arrays.fill(temp, 1);
-        int zeros = 0, twos = n-1;
+        int zeros = 0, ones = 0, twos = 0;
 
         for(int i = 0; i < n; i++){
             if(nums[i] == 0){
-                temp[zeros] = 0;
                 zeros++;
             }
-            else if(nums[i] == 2){
-                temp[twos] = 2;
-                twos--;
+            else if(nums[i] == 1){
+                ones++;
+            }
+            else{
+                twos++;
             }
         }
 
-        //copy back all items from temp[] to original nums[]
-        for(int i = 0; i < n; i++){
-            nums[i] = temp[i];
+        //copy back count of 0s, 1s & 2s to original nums[]
+        int x = 0;
+        for(int j = 0; j < zeros; j++){
+            nums[x++] = 0;
+        }
+
+        for(int j = 0; j < ones; j++){
+            nums[x++] = 1;
+        }
+
+        for(int j = 0; j < twos; j++){
+            nums[x++] = 2;
         }
     }
 }
