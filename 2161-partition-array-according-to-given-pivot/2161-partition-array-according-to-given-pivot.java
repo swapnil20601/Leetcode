73a0d2420b97817a6debb9f420b2085cfa6d3905
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2026-05-23 20:48:59 -0500

    Time: 5 ms (73.93%), Space: 160.2 MB (21.1%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
@@ -2,26 +2,31 @@ class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
         int n = nums.length;
         int[] temp = new int[n];
-        int x = 0;
+        int lessThanPivot = 0, equalToPivot = 0;
 
         for(int i = 0; i < n; i++){
             if(nums[i] < pivot){
-                temp[x++] = nums[i];
+                lessThanPivot++;
+            }
+            else if(nums[i] == pivot){
+                equalToPivot++;
             }
         }
+
+        int left = 0, mid = lessThanPivot, right = lessThanPivot + equalToPivot;
 
         for(int i = 0; i < n; i++){
-            if(nums[i] == pivot){
-                temp[x++] = nums[i];
+            if(nums[i] < pivot){
+                temp[left++] = nums[i];
+            }
+            else if(nums[i] == pivot){
+                temp[mid++] = nums[i];
+            }
+            else{
+                temp[right++] = nums[i];
             }
         }
-
-        for(int i = 0; i < n; i++){
-            if(nums[i] > pivot){
-                temp[x++] = nums[i];
-            }
-        }
-
+        
         return temp;
     }
 }
