48141305053030424de03776d5d22fb7488a7aa6
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2026-05-24 15:32:40 -0500

    Time: 16 ms (21.61%), Space: 51.3 MB (89.83%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -2,23 +2,18 @@ class Solution {
     public List<Integer> majorityElement(int[] nums) {
         List<Integer> res = new ArrayList<>();
         int n = nums.length;
-        
-        for(int i = 0; i < n; i++){
-            if(res.contains(nums[i])){
-                continue;
-            }
-            int count = 0;
-            for(int j = 0; j < n; j++){
-                if(nums[j] == nums[i]){
-                    count++;
-                }
-            }
-            if(count > n/3){
-                res.add(nums[i]);
-            }
-            //If the list contains 2 elements, we are done. Becoz we know at max there can be only 2 candidates whose frequncy is > n/3
-            if(res.size() == 2){
-                break;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int x : nums){
+            map.put(x, map.getOrDefault(x,0)+1);
+        }
+
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+            int key = entry.getKey();
+            int val = entry.getValue();
+
+            if(val > n/3){
+                res.add(key);
             }
         }
 
