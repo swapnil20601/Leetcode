0516c4b93f4d36c0a782190d170d30970586c1a1
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2026-05-24 15:42:50 -0500

    Time: 2 ms (99.94%), Space: 52.5 MB (64.35%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -2,21 +2,51 @@ class Solution {
     public List<Integer> majorityElement(int[] nums) {
         List<Integer> res = new ArrayList<>();
         int n = nums.length;
-        Map<Integer, Integer> map = new HashMap<>();
-
-        for(int x : nums){
-            map.put(x, map.getOrDefault(x,0)+1);
-        }
-
-        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
-            int key = entry.getKey();
-            int val = entry.getValue();
-
-            if(val > n/3){
-                res.add(key);
+        int candidate1 = Integer.MIN_VALUE;
+        int candidate2 = Integer.MIN_VALUE;
+        int count1 = 0;
+        int count2 = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] == candidate1){
+                count1++;
+            }
+            else if(nums[i] == candidate2){
+                count2++;
+            }
+            else if(count1 == 0){
+                candidate1 = nums[i];
+                count1 = 1;
+            }
+            else if(count2 == 0){
+                candidate2 = nums[i];
+                count2 = 1;
+            }
+            else{
+                count1--;
+                count2--;
             }
         }
 
+        int freq1 = verifyCandidateCount(nums,candidate1);
+        int freq2 = verifyCandidateCount(nums,candidate2);
+
+        if(freq1 > n/3){
+            res.add(candidate1);
+        }
+        if(freq2 > n/3){
+            res.add(candidate2);
+        }
+
         return res;
+    }
+
+    private int verifyCandidateCount(int[] arr, int x){
+        int count = 0;
+        for(int ele : arr){
+            if(ele == x){
+                count++;
+            }
+        }
+        return count;
     }
 }
