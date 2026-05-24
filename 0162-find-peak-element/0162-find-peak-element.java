fdec7c1db20716f906725c152b179280e15edce1
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2026-05-24 09:32:04 -0500

    Time: 0 ms (100%), Space: 44.2 MB (54.75%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -6,12 +6,22 @@ class Solution {
         //Edge cases: Check if first & last element of array can form a peak
         if(nums[0] > nums[1]) return 0;
         if(nums[n-1] > nums[n-2]) return n-1;
+        
+        int low = 1, high = n-2;
 
-        for(int i = 1; i < n-1; i++){
-            if((nums[i-1] < nums[i]) && (nums[i+1] < nums[i])){
-                return i;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if((nums[mid] > nums[mid-1]) && (nums[mid] > nums[mid+1])){
+                return mid;
+            }
+            else if((nums[mid] > nums[mid-1]) && (nums[mid] < nums[mid+1])){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
             }
         }
+
         return -1;
     }
 }
