4d04309443c7d29db5f958f995b6da155e286ef3
Swapnil, Jadhav(20601) <55676207+swapnil20601@users.noreply.github.com>
2026-05-30 20:56:22 -0500

    Time: 5 ms (36.2%), Space: 42.9 MB (8.84%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -1,23 +1,10 @@
 class Solution {
     public int arrangeCoins(int n) {
-        int low = 1;
-        int high = n;
-        int ans = -1;
-        while(low <= high){
-            int mid = low+(high-low)/2;
-            if(isPossible(mid,n)){
-                ans = mid;
-                low = mid+1;
-            }
-            else{
-                high = mid-1;
-            }
+        int i = 1;
+        while(i <= n){
+            n = n - i;
+            i++;
         }
-        return ans;
-    }
-
-    private boolean isPossible(int row, int n){
-        long number_of_coins = (long) row*(row+1)/2;
-        return number_of_coins <= n;
+        return i-1;
     }
 }
